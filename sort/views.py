4c4cb8ad9e660b7602951bbd6d979a3d18bf12c7
justin890219 <67851111+justin890219@users.noreply.github.com>
2022-05-31 21:54:31 +0800

    Add files via upload

---
+++ sort/views.py
@@ -0,0 +1,51 @@
+
+from django.shortcuts import render
+import csv
+# Create your views here.
+
+
+def home(request):
+    return render(request, "index.html")
+
+
+def sort1(request):
+    with open(r'C:\\output.csv', newline='', encoding='utf-8') as csvfile:
+        rows = csv.reader(csvfile)
+        results = []
+        for row in rows:
+            content, date, likes, comments, link = row
+            results.append(dict(content=content, date=date,
+                           likes=likes, comments=comments, link=link))
+    return render(request, "sort1.html", {'results': results})
+
+
+def sort2(request):
+    with open(r'C:\\output.csv', newline='', encoding='utf-8') as csvfile:
+        rows = csv.reader(csvfile)
+        results = []
+        for row in rows:
+            content, date, likes, comments, link = row
+            if likes == "":
+                likes = "0"
+            if comments == "":
+                comments = "0"
+            results.append(dict(content=content, date=date,
+                           likes=likes, comments=comments, link=link))
+            results.sort(key=lambda x: int(x["likes"]), reverse=True)
+    return render(request, "sort2.html", {'results': results})
+
+
+def sort3(request):
+    with open(r'C:\\output.csv', newline='', encoding='utf-8') as csvfile:
+        rows = csv.reader(csvfile)
+        results = []
+        for row in rows:
+            content, date, likes, comments, link = row
+            if likes == "":
+                likes = "0"
+            if comments == "":
+                comments = "0"
+            results.append(dict(content=content, date=date,
+                           likes=likes, comments=comments, link=link))
+            results.sort(key=lambda x: int(x["comments"]), reverse=True)
+    return render(request, "sort3.html", {'results': results})
